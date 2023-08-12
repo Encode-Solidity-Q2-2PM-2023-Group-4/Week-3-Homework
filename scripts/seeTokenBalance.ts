ec919fbaa5d6b8d1276ceadfc74e93dc919d85af
Nandinho42069 <129112008+Nandinho42069@users.noreply.github.com>
2023-08-12 19:07:28 +0100

    Create seeTokenBalance.ts

---
+++ scripts/seeTokenBalance.ts
@@ -0,0 +1,35 @@
+import { ethers } from "ethers";
+import { MyToken, MyToken__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+import * as BallotJSON from "../artifacts/contracts/TokenizedBallot.sol/TokenizedBallot.json";
+import * as TokenJSON from "../artifacts/contracts/ERC20Votes.sol/MyToken.json";
+dotenv.config();
+
+const MINT_VALUE = ethers.parseUnits("1");
+
+function setupProvider(){
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+  return provider;
+}
+
+async function main() {
+  // Define provider and wallet
+  const provider = setupProvider();
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+  const signer = wallet.connect(provider);
+    
+  // Attach
+  const ballotContract = new ethers.Contract("<BALLOT ADDRESS GOES HERE>", BallotJSON.abi, signer);
+  const tokenContractFactory = new MyToken__factory(signer);
+  const tokenContract = tokenContractFactory.attach("<TOKEN ADDRESS GOES HERE>") as MyToken;
+
+  // Check balances and return value
+  const balanceBN = await tokenContract.balanceOf(signer.address);
+  console.log(`Account ${signer.address} has ${balanceBN.toString()} decimal units of MyToken\n`);
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
