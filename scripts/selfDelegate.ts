1bf8b4bfc525d9c2063e8a362abe249d40aed0e5
Nandinho42069 <129112008+Nandinho42069@users.noreply.github.com>
2023-08-12 18:18:00 +0100

    Update selfDelegate.ts

--- scripts/selfDelegate.ts
+++ scripts/selfDelegate.ts
@@ -1,1 +1,47 @@
+import { ethers } from "ethers";
+import { MyToken, MyToken__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+import * as BallotJSON from "../artifacts/contracts/TokenizedBallot.sol/Ballot.json";
+import * as TokenJSON from "../artifacts/contracts/ERC20Votes.sol/MyToken.json";
+dotenv.config();
 
+const MINT_VALUE = ethers.parseUnits("1");
+
+function setupProvider(){
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+  return provider;
+}
+
+async function main() {
+  
+  // Define provider and wallet
+  const provider = setupProvider();
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+  const signer = wallet.connect(provider);
+
+  // Attach
+  const ballotContract = new ethers.Contract("<BALLOT ADDRESS GOES HERE>", BallotJSON.abi, signer);
+  const tokenContractFactory = new MyToken__factory(signer);
+  const tokenContract = tokenContractFactory.attach("<TOKEN ADDRESS GOES HERE>") as MyToken;
+
+  // Check the voting power
+  const votes = await tokenContract.getVotes(signer.address);
+  console.log(`Account ${signer.address} has ${votes.toString()} units of voting power before self delegating\n`);
+
+  // Self delegate
+  const delegateTx = await tokenContract.connect(signer).delegate(signer.address);
+  await delegateTx.wait();
+
+  // Return the voting power after delegation
+  const votesAfter = await tokenContract.getVotes(signer.address);
+  console.log(`Account ${signer.address} has ${votesAfter.toString()} units of voting power after self delegating\n`);
+
+
+}
+    
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
